b20fe689b352609c72b86710827609ab7c495ea1
MarcinHaduch <haduch1996@gmail.com>
2019-05-21 15:48:02 +0200

    Simple Hello world

---
+++ Task 1 hello world/main.py
@@ -0,0 +1,7 @@
+def main():
+    first_name = input("Provide me your first name\n")
+    last_name = input("Last name\n")
+    print(f"Hello, {first_name} {last_name}")
+
+if __name__ == "__main__":
+    main()
